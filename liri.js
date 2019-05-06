51522f32bbf692a34c4993b25c0df62bc9dd01cc
CERIO18 <ceurioste@derp.org>
2019-05-06 17:27:35 -0600

    three out of four work

--- liri.js
+++ liri.js
@@ -2,18 +2,9 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
-
-
-// node liri.js movie-this '<movie name here>'
-
-// info for request
 var request = require("request");
-
 var moment = require("moment");
-
-// Info for fs
 var fs = require("fs");
-
 var command = process.argv[2];
 
 function movieThis(movieTitle) {
@@ -25,8 +16,6 @@ function movieThis(movieTitle) {
   request("https://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy", function (error, response, data) {
     if (!error && response.statusCode === 200) {
       console.log(data);
-      console.log("");
-
       console.log("Title: " + JSON.parse(data).Title);
       console.log("Year Released: " + JSON.parse(data).Year);
       console.log("IMDB Rating: " + JSON.parse(data).imdbRating);
@@ -47,13 +36,10 @@ function movieThis(movieTitle) {
       } else {
         console.log('Rotten Tomatoes rating not found');
       }
-
       console.log("Country Produced: " + JSON.parse(data).Country);
       console.log("Language: " + JSON.parse(data).Language);
       console.log("Plot: " + JSON.parse(data).Plot);
       console.log("Actors: " + JSON.parse(data).Actors);
-      console.log("");
-
     }
   });
 };
@@ -61,14 +47,9 @@ function movieThis(movieTitle) {
 function concertThis(nameArtist) {
   request("https://rest.bandsintown.com/artists/" + nameArtist + "/events?app_id=codingbootcamp", function (error, response, data) {
     if (!error && response.statusCode === 200) {
-
-
       console.log("Venue: " + JSON.parse(data)[0].venue.name);
       console.log("Location: " + JSON.parse(data)[0].venue.city + " " + JSON.parse(data)[0].venue.region);
-      console.log("Date: " + moment(JSON.parse(data)[0].datetime).format("MM/DD/YYYY"));
-
-      console.log("");
-    
+      console.log("Date: " + moment(JSON.parse(data)[0].datetime).format("MM/DD/YYYY"));    
     }
 
   });
@@ -81,48 +62,32 @@ function songThis(song) {
   spotify.search({ type: "track", query: song }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
-
     }
-
-    console.log("");
-
     console.log("Artist: " + data.tracks.items[0].artists[0].name);
     console.log("Song Name: " + data.tracks.items[0].name);
     console.log("Preview Link: " + data.tracks.items[0].preview_url);
     console.log("Album: " + data.tracks.items[0].album.name);
-
-    console.log("");
-
   });
 }
 
-// movie
 if (command === "movie-this") {
-
   var movie = process.argv[3];
   movieThis(movie);
 
 }
 
-
 else if (command === "concert-this") {
-
   var artist = process.argv[3];
   concertThis(artist);
 }
 
-
 else if (command === "spotify-this-song") {
   var song = process.argv[3];
-
   songThis(song);
 }
 
-
 else if (command === "do-what-it-says") {
-
-  fs.readFile("random.txt", "utf8", function (error, data) {
-
+  fs.readFile("random.txt", function (error, data) {
     if (error) {
       return console.log(error);
     }
@@ -143,7 +108,6 @@ else if (command === "do-what-it-says") {
       movieThis(movie);
 
     } else if (command === "spotify-this-song") {
-
       var song = doWhatItSays;
       songThis(song);
 
@@ -155,23 +119,16 @@ else if (command === "do-what-it-says") {
           return console.log('Error occurred: ' + err);
 
         }
-        console.log("");
-        console.log("");
-
         console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Song Name: " + data.tracks.items[0].name);
         console.log("Preview Link: " + data.tracks.items[0].preview_url);
         console.log("Album: " + data.tracks.items[0].album.name);
-
-        console.log("");
        
       });
-
     }
   });
 } else {
   console.log("Error!")
 }
-
 
 
